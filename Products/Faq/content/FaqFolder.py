7800d623a76387a3a6a139bc18724cca68146672
i-zone <hello@i-zone.de>
2013-07-19 15:25:29 +0200

    richtext field

--- Products/Faq/content/FaqFolder.py
+++ Products/Faq/content/FaqFolder.py
@@ -4,7 +4,8 @@ from interfaces import IFaqFolder
 from zope.interface import implements
 from Products.ATContentTypes.atct import ATFolder, ATFolderSchema
 from Products.Archetypes.public import IntegerField, Schema, \
-                                       IntegerWidget, registerType
+                                       IntegerWidget, registerType, \
+                                       TextField, RichWidget
 
 from Products.Faq import config
 from Products.Faq import faqMessageFactory as _
@@ -13,18 +14,28 @@ from Products.Faq import faqMessageFactory as _
 schema = ATFolderSchema.copy() + Schema((
 
     IntegerField('delay',
-                 widget=IntegerWidget(description=_(u"desc_delay", 
+                 widget=IntegerWidget(description=_(u"desc_delay",
                                              default=u"Delay for a new item."),
                                       label=_(u"label_delay", default=u"Delay")
                         ),
-                 default=7,
+                 default=0,
                  required=1,
                  searchable=0,
                  validators=('isInt',)),
+
+    TextField("text",
+        required=0,
+        searchable=1,
+        default_output_type="text/html",
+        allowable_content_types=("text/html",
+                                 "text/plain",
+                                 "text/restructured"),
+        widget  = RichWidget,
+        ),
     ))
 
 schema['description'].widget.label = _(u"label_folder", default=u"Description")
-schema['description'].widget.description = _(u"desc_folder", 
+schema['description'].widget.description = _(u"desc_folder",
                                   default=u"The description of the FAQ category.")
 
 class FaqFolder(ATFolder):
